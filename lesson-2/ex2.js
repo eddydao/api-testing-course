9449ab5594f9de2a8107845290fb02f2e2ca74f0
Eddy <daokythanh@gmail.com>
2025-03-11 15:10:20 +0700

    add result of ex2 and ex3 when run in jsfiddle

--- lesson-2/ex2.js
+++ lesson-2/ex2.js
@@ -1,9 +1,4 @@
-const myName = 'Dao Ky Thanh';
-const myAddress = 'Hoang Mai, Hanoi';
-const myFavorites = 'Coding';
-const isLearningAPI = true;
-
-console.log('myName: ', myName);
-console.log('myAddress: ', myAddress);
-console.log('myFavorites: ', myFavorites);
-console.log('isLearningAPI: ', isLearningAPI);
+"myName: ", "Dao Ky Thanh"
+"myAddress: ", "Hoang Mai, Hanoi"
+"myFavorites", "Coding"
+"isLearningAPI: ", true
--- lesson-2/ex3.js
+++ lesson-2/ex3.js
@@ -1,6 +1,1 @@
-const height = 175;
-const optimalWeight = 75 * 9 / 10;
-const maxWeight = 75;
-const minimalWeight = 75 * 8 / 10;
-
-console.log(optimalWeight + ' ' + maxWeight + ' ' + minimalWeight);
+"67.5 75 60"
