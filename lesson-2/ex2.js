07505a52afc197352cb19e25509f48574148ce41
Eddy Lockwood <doakythanh@gmail.com>
2025-03-16 21:22:37 +0700

    add missing exe

--- lesson-2/ex2.js
+++ lesson-2/ex2.js
@@ -1,3 +1,13 @@
+let myName = "Dao Ky Thanh"
+let myAddress = "Hoang Mai, Hanoi"
+let myFavorites = "Coding"
+let isLearningAPI = true
+
+console.log("myName: ", myName)
+console.log("myAddress: ", myAddress)
+console.log("myFavorites", myFavorites)
+console.log("isLearningAPI: ", isLearningAPI)
+
 "myName: ", "Dao Ky Thanh"
 "myAddress: ", "Hoang Mai, Hanoi"
 "myFavorites", "Coding"
